da43c65a2526af3141013c4ec583b540335969f9
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-23 18:49:01 +0300

    Update InternetBank.java

--- src/main/java/ru/bank/InternetBank.java
+++ src/main/java/ru/bank/InternetBank.java
@@ -2,7 +2,6 @@ package ru.bank;
 import ru.models.Order;
 import ru.models.Product;
 import java.util.List;
-
 public interface InternetBank {
     Order createOrder(Integer productId, String customer) throws Exception;
     Order payment(Integer orderId) throws Exception;
