83abf0861dfa8deb014046e4c62ea99e562f397e
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-23 18:50:08 +0300

    Update LevelOperationEnum.java

--- src/main/java/ru/enums/LevelOperationEnum.java
+++ src/main/java/ru/enums/LevelOperationEnum.java
@@ -1,4 +1,5 @@
 package ru.enums;
+
 public enum LevelOperationEnum{
     Medium("Операция, представленая для граждан с низкими доходами"), Standart("Стандартная операция для доходоспособных граждан"), Gold("Операция, при которой проходит огромный денежный оборот");
     private final String value;
