3606a40a816a3ecba9235250573f5c38acdb5e23
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-23 13:23:01 +0300

    Create Consumer.java

---
+++ src/main/java/ru/models/Consumer.java
@@ -0,0 +1,27 @@
+package ru.models;
+
+public class Consumer extends Model{
+    String name;
+    Double money;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getMoney() {
+        return money;
+    }
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+    public Consumer(Integer id, String name, Double money) {
+        super(id);
+        this.name = name;
+        this.money = money;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + "; name: " + name + "; money: " + money + "\n";
+    }
+}
