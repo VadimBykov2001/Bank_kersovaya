6eaca280f3180614ba284550662f71c63828704f
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-23 13:01:21 +0300

    Create InternetBank.java

---
+++ src/main/java/ru/bank/InternetBank.java
@@ -0,0 +1,12 @@
+package ru.bank;
+import ru.models.Order;
+import ru.models.Product;
+import java.util.List;
+
+public interface InternetBank {
+    Order createOrder(Integer productId, String customer) throws Exception;
+    Order payment(Integer orderId) throws Exception;
+    Order returnProduct(Integer orderId) throws Exception;
+    Order modifOrder(Integer orderId, Integer productId) throws Exception;
+    List<Product> catalog();
+}
