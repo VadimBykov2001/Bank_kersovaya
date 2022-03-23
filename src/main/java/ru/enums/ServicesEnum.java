551efa2804ba9518e753a16486590a8f074fb913
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-23 17:23:44 +0300

    Update and rename BankProductsEnum.java to ServicesEnum.java

--- src/main/java/ru/enums/ServicesEnum.java
+++ src/main/java/ru/enums/ServicesEnum.java
@@ -1,9 +1,9 @@
 package ru.enums;
 
-public enum BankProductsEnum{
+public enum ServicesEnum{
     Credit("Кредит"), CreditCard("Кредитная карта"), Investment("Инвестиции"), Insurance("Страхование"), Mortgage("Ипотека");
     private final String value;
-    BankProductsEnum(String value) {
+    ServicesEnum(String value) {
         this.value = value;
     }
     public String getValue() {
